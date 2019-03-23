1b04b8c83ba6f4620fd2c43d30aa2351c73067c9
Prasanth <31370970+prasanthse@users.noreply.github.com>
2019-03-23 13:57:41 +0530

    SideNav component created

--- PortFolio/src/app/app-routing.module.ts
+++ PortFolio/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from 'src/app/Components/home/home.component';
+import { SideNavComponent } from 'src/app/Components/side-nav/side-nav.component';
 
 const routes: Routes = [
   {
--- PortFolio/src/app/app.module.ts
+++ PortFolio/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from 'src/app/Components/home/home.component';
-import { HeaderComponent } from 'src/app/Components//header/header.component';
+import { HeaderComponent } from 'src/app/Components/header/header.component';
+import { SideNavComponent } from 'src/app/Components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
