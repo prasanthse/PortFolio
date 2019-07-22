923eef97538f21caa2dbc13a058c5136c7facef7
Prasanth <31370970+prasanthse@users.noreply.github.com>
2019-07-22 16:04:13 +0530

    All unwanted cleared in admin

--- ngx-admin-master/src/app/pages/extra-components/extra-components-routing.module.ts
+++ ngx-admin-master/src/app/pages/extra-components/extra-components-routing.module.ts
@@ -2,11 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ExtraComponentsComponent } from './extra-components.component';
-import { AlertComponent } from './alert/alert.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { SpinnerComponent } from './spinner/spinner.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { ChatComponent } from './chat/chat.component';
 import { CalendarKitFullCalendarShowcaseComponent } from './calendar-kit/calendar-kit.component';
 
 const routes: Routes = [{
@@ -14,28 +9,8 @@ const routes: Routes = [{
   component: ExtraComponentsComponent,
   children: [
     {
-      path: 'calendar',
-      component: CalendarComponent,
-    },
-    {
-      path: 'progress-bar',
-      component: ProgressBarComponent,
-    },
-    {
-      path: 'spinner',
-      component: SpinnerComponent,
-    },
-    {
-      path: 'alert',
-      component: AlertComponent,
-    },
-    {
       path: 'calendar-kit',
       component: CalendarKitFullCalendarShowcaseComponent,
-    },
-    {
-      path: 'chat',
-      component: ChatComponent,
     },
   ],
 }];
--- ngx-admin-master/src/app/pages/tables/tables.module.ts
+++ ngx-admin-master/src/app/pages/tables/tables.module.ts
@@ -4,7 +4,6 @@ import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { TablesRoutingModule, routedComponents } from './tables-routing.module';
-import { FsIconComponent } from './tree-grid/tree-grid.component';
 
 @NgModule({
   imports: [
@@ -17,8 +16,7 @@ import { FsIconComponent } from './tree-grid/tree-grid.component';
     Ng2SmartTableModule,
   ],
   declarations: [
-    ...routedComponents,
-    FsIconComponent,
+    ...routedComponents
   ],
 })
 export class TablesModule { }
